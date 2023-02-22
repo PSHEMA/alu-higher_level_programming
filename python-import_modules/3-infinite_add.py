b0b52354bc1ed0150fd33308914bf8660cd228d9
Placide SHEMA <114446260+PSHEMA@users.noreply.github.com>
2023-02-22 13:23:04 +0200

    Update 3-infinite_add.py

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -2,12 +2,12 @@
 from sys import argv
 
 if __name__ == '__main__':
-    args_number = len(args) - 1
+    args_number = len(argv) - 1
 
     if args_number > 0:
         sum_ = 0
         for args in argv[1:]:
-            sum_ = sum_ + int(args)
+            sum_ += int(args)
         print("{}".format(sum_))
     else:
         print("{}".format(args_number))
