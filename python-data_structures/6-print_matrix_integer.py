0421a9e73d2117f52dd16fc601f0a9c11752c378
Placide SHEMA <114446260+PSHEMA@users.noreply.github.com>
2023-03-02 12:49:29 +0200

    Update 6-print_matrix_integer.py

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        for elem in row:
-            print("{:d}".format(elem), end= " ")
-    print()
+        for col in row:
+            print("{:d}".format(col), end=" " if col != row[-1] else "")
+        print()
