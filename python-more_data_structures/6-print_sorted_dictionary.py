cfd67cdf20a48215170dd0c3a047333e8cc84ca2
PSHEMA <p.shema@alustudent.com>
2023-03-06 06:09:33 -0800

    Task 5

---
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def print_sorted_dictionary(a_dictionary):
+    sorted_items = sorted(a_dictionary.items())
+    for key, value in sorted items:
+        print(f"{key}: {value}")
