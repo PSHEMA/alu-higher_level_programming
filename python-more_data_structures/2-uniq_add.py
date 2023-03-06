ba717ae9ed054372c77e41690d30d9667536b24b
PSHEMA <p.shema@alustudent.com>
2023-03-06 04:57:09 -0800

    Task 2

---
+++ python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    unique_set = set()
+    for element in my_list:
+        if element not in unique_set:
+            unique_set.add(element)
+            rsult = result + element
+    return result
