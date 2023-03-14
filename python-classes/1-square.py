321390f2883df4078b693f9757db43ccb81e0090
PSHEMA <p.shema@alustudent.com>
2023-03-14 01:15:27 -0700

    Task 1

---
+++ python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""A class Square that defines a square"""
+
+
+class Square:
+    """
+    This class defines a square with a private instance attribute `size`.
+    """
+    def __init__(self, size):
+        self.__size = size
