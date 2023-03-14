b7c68347fd734892d44af011f5efcd9914f80311
PSHEMA <p.shema@alustudent.com>
2023-03-14 03:24:50 -0700

    Task 2

---
+++ python-more_classes/2-rectangle.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+"""A class Rectangle that defines a rectangle"""
+
+
+class Rectangle:
+    """Creates a rectangle"""
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """getter for width"""
+        return self.__width
+    
+    @width.setter
+    def width(self, value):
+        """setter for width"""
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+        
+    @property
+    def height(self):
+        """getter for height"""
+        return self.__height
+    
+    @height.setter
+    def height(self, value):
+        """setter for height"""
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
+
+    def area(self):
+        """Returns the rectangle area"""
+        return (self.__width*self.__height)
+
+    def perimeter(self):
+        """Returns the perimeter of the rectangle"""
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        else:
+            return (self.__width+self.__height)*2
