25dc2c7d13ad540c475dc5eade19c3cb1388f82b
PSHEMA <p.shema@alustudent.com>
2023-03-14 01:43:19 -0700

    Task 4

---
+++ python-classes/4-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""A class Square that defines a square"""
+
+
+class Square:
+    """
+    This class defines a square with a private instance attribute `size`.
+    """
+    def __init__(self, size=0):
+        self.size = size
+
+    @property
+    def size(self):
+        """Getter for size."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Setter for size."""
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Returns the area of the square"""
+        return (self.__size * self.__size)
