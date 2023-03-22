2cb78221f555c146f474afbc2b8e324e7a072ecb
PSHEMA <p.shema@alustudent.com>
2023-03-22 12:56:40 -0700

    Task 9

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Inherits from BaseGeometry"""
+
+
+BaseGeometry = __import__('7-base_geomrtry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """class Rectangle inheritrd from BaseGeometry"""
+    def __init__(self. width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """returns the are of the instance"""
+        return self.__width*self.__height
+
+    def __str__(self):
+        """return a printable string"""
+        return "[Rectangle] {:d}/{:d}".format(self.__width, self.__height)
