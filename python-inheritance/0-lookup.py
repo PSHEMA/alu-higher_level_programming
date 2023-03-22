71c6d9c32d8be95a37719e5f9381ff7c12498541
PSHEMA <p.shema@alustudent.com>
2023-03-22 04:25:49 -0700

    Lookup

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""A function that returns the list of available attributes and methods of an object"""
+
+
+def lookup(obj):
+    """Retruns the list available attributes and methods"""
+    return dir(obj)
