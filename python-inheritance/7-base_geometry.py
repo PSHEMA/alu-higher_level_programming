769f45e2bc1db90a17f916899e0643c833f0dade
Placide SHEMA <114446260+PSHEMA@users.noreply.github.com>
2023-03-22 20:55:21 +0200

    Task 7

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,14 +1,23 @@
 #!/usr/bin/python3
-"""A class BaseGeometry"""
+""" Class that defines the attributes of Geometric Shapes """
 
 
 class BaseGeometry:
-    """class BaseGeometry"""
+    """ Class that defines the attributes of Geometric Shapes """
+
     def area(self):
+        """ Method that defines the area of a geomtric shape """
+
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        if type(value) != int:
-            raise TypeError("<name> must be an integer")
-        if value =< 0:
-            raise ValueError("<name> must be greater than 0")
+        """ Method that recieves the value property
+        Árgs:
+            name: name of the object
+            value: value of the property
+        """
+
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
