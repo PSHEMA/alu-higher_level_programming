6a5ba5b83c77f11424d45aacde0059dcfa909677
Placide SHEMA <114446260+PSHEMA@users.noreply.github.com>
2023-03-22 13:59:12 +0200

    Update 2-is_same_class.py

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -4,7 +4,5 @@
 
 def is_same_class(obj, a_class):
     """True or False"""
-    if isinstance(obj, a_class):
-        return True
-    else:
-        return False
+    def is_same_class(obj, a_class):
+    return isinstance(obj, a_class)
