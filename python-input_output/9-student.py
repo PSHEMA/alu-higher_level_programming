8e6d40b8fd70e7306112f667c5e10c8c024624a1
Placide SHEMA <114446260+PSHEMA@users.noreply.github.com>
2023-03-24 12:54:00 +0200

    Update 9-student.py

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -4,11 +4,11 @@
 
 class Student:
     """A class Student"""
+    
     def __init__(self, first_name, last_name, age):
-        """Initializes instance"""
-        self.__first_name = first_name
-        self.__last_name = last_name
-        self.__age = age
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
 
     def to_json(self):
         """retrieves a dictionary representation"""
