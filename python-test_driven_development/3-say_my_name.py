ed666299f27c32950f32ce52fcda83cee966bca4
Placide SHEMA <114446260+PSHEMA@users.noreply.github.com>
2023-04-13 15:12:36 +0200

    PTDD

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """A function that prints A name"""
 
+
 def say_my_name(first_name, last_name=""):
+    """A name function"""
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
