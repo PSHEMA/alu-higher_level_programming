5c6357c3526e62bd163dea7941249c8a5ca3b234
Placide SHEMA <114446260+PSHEMA@users.noreply.github.com>
2023-04-13 15:19:34 +0200

    PTDD

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 """A function that adds twwo integers"""
 
+
 def add_integer(a, b=98):
+    """An addition function"""
     if not isinstance(a, (int, float)):
-        raise TypeError("a must be an integer or a float")
+        raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
-        raise TypeError("b must be an integer or a float")
+        raise TypeError("b must be an integer")
     a = int(a)
     b = int(b)
     return a + b
