a8a58257c3061d1b0693a0cc1ab897cd51a490c1
PSHEMA <p.shema@alustudent.com>
2023-04-24 03:53:20 -0700

    ORM

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""List all states from the database"""
+
+import sys
+import MySQLdb
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(user=sys.argv[1], passwwd=sys.argv[2], deb=sys.argv[3], port=3306)
+
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states;")
+    states = cur.fetchall()
+
+    for state in states:
+        print(state)
