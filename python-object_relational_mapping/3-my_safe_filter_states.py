25c88fc9a12286be0592224291fff963e3ea382e
PSHEMA <p.shema@alustudent.com>
2023-04-26 04:54:06 -0700

    ORM

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Display values in states table
+"""
+import sys
+import MySQLdb
+
+if __name_ == '__main__':
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
+                         db=sys.argv[3], port=3306)
+
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name = %s;", (sys.argv[4],))
+    states = cur.fetchall()
+
+    for state in states:
+        print(state)
