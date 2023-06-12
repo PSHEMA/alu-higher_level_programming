e930c3dee7bad80d55f74987064f03b58de4a947
Placide SHEMA <114446260+PSHEMA@users.noreply.github.com>
2023-06-12 13:53:24 +0200

    JS

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -1,12 +1,9 @@
 #!/usr/bin/node
 class Rectangle {
   constructor (w, h) {
-    if (Number.isInteger(w) && Number.isInteger(h) && w > 0 && h > 0) {
+    if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
-    } else {
-      this.width = 0;
-      this.height = 0;
     }
   }
 }
