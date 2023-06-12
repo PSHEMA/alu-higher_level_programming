23597c5e4bf9f0e4e615f3d89660bb87d108053a
Placide SHEMA <114446260+PSHEMA@users.noreply.github.com>
2023-06-12 14:32:03 +0200

    JS

--- javascript_objects_scopes_closures/5-square.js
+++ javascript_objects_scopes_closures/5-square.js
@@ -1,14 +1,8 @@
 #!/usr/bin/node
-
 const Rectangle = require('./4-rectangle');
-
 class Square extends Rectangle {
-  constructor(size) {
-    if (typeof size !== 'number' || size <= 0 || !Number.isInteger(size)) {
-      throw new Error('Invalid size. Size must be a positive integer.');
-    }
+  constructor (size) {
     super(size, size);
   }
 }
-
 module.exports = Square;
