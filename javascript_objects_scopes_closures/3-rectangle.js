5e3a75d0945d019d4985009c9e2b22861959689e
PSHEMA <p.shema@alustudent.com>
2023-06-12 04:38:18 -0700

    JS

---
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor(w, h) {
+    if (Number.isInteger(w) && Number.isInteger(h) && w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    } else {
+      this.width = 0;
+      this.height = 0;
+    }
+  }
+
+  print() {
+    if (this.width === 0 || this.height === 0) {
+      console.log('Empty Rectangle');
+      return;
+    }
+
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+}
+module.exports = Rectangle;
---
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,34 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor(w, h) {
+    if (Number.isInteger(w) && Number.isInteger(h) && w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    } else {
+      this.width = 0;
+      this.height = 0;
+    }
+  }
+
+  print() {
+    if (this.width === 0 || this.height === 0) {
+      console.log("Empty rectangle");
+    } else {
+      for (let i = 0; i < this.height; i++) {
+        console.log("X".repeat(this.width));
+      }
+    }
+  }
+
+  rotate() {
+    [this.width, this.height] = [this.height, this.width];
+  }
+
+  double() {
+    if (this.width !== 0 && this.height !== 0) {
+      this.width *= 2;
+      this.height *= 2;
+    }
+  }
+}
+module.exports = Rectangle;
